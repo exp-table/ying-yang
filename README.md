4ea9140ce4853d9ed0bf1c5d228dd2df025437b3
exp <exp.table@protonmail.com>
2022-01-01 23:55:47 +0100

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ying-yang
+# Ying-Yang
 Pay on Ethereum, mint on Starknet.
+
+Although this is part of my experiments with Starknet to get more familiar with it, I believe it could potentially be a solution for a (temporary, hopefully) problem on Starknet. To paliate for lack of support for wrapped eth or other currencies, we split the payment on Ethereum and the minting itself on Starknet.
+
+## Payment contract on Ethereum
+A simple contract that accepts eth as payment (and refunds it of course, since it's on Goerli). It works by constructing the proper payload for Starknet which is made of the contract's address on Starknet, the selector of the mint function and the recipient's address.
+
+If you are wondering why the recipient is a `uint256` type and not `address`, it's because we expect users to be represented by smart-contracts on Starknet.
+
+You can interact with the contract [here](https://goerli.etherscan.io/address/0xc1c54da1676a16223474fb1557be295959b20038).
+
+It expects a 0.001 eth payment everytime you mint.
+
+## Minting on Starknet
+The contract is an extremely simplified version of the `ERC721`. It just mints a single tokenId to an address and ... that's it. Add two view functions, ` totalSupply` and `ownerOf`to it, and it's done! Ready to be consumed!
+
+You can interact with the contract [here](https://goerli.voyager.online/contract/0x06d752ffe2c6ae926b473937d855051423df4f923ef8ec6cc7fea9115812179a#readContract).
---
+++ contracts/VendingMachine.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.8.10;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+    Taken from https://cairo-lang.org/docs/_static/L1L2Example.sol
+*/
+
+interface IStarknetCore {
+    /**
+      Sends a message to an L2 contract.
+
+      Returns the hash of the message.
+    */
+    function sendMessageToL2(
+        uint256 to_address,
+        uint256 selector,
+        uint256[] calldata payload
+    ) external returns (bytes32);
+
+    /**
+      Consumes a message that was sent from an L2 contract.
+
+      Returns the hash of the message.
+    */
+    function consumeMessageFromL2(uint256 fromAddress, uint256[] calldata payload)
+        external
+        returns (bytes32);
+}
+
+contract VendingMachine is Ownable {
+    // The StarkNet core contract.
+    IStarknetCore starknetCore;
+
+    uint256 public constant PRICE = 0.001 ether;
+    uint256 public l2Address;
+
+    // The selector of the "mint" l1_handler.
+    uint256 constant MINT_SELECTOR =
+        1329909728320632088402217562277154056711815095720684343816173432540100887380;
+
+    /**
+      Initializes the contract state.
+    */
+    constructor(IStarknetCore starknetCore_) payable {
+        starknetCore = starknetCore_;
+    }
+
+    function setL2Address(uint256 l2Address_) public onlyOwner {
+        l2Address = l2Address_;
+    }
+
+    function pay(
+        uint256 user
+    ) external payable {
+        require(msg.value == PRICE);
+        // Construct the mint message's payload.
+        uint256[] memory payload = new uint256[](1);
+        payload[0] = user;
+
+        // Send the message to the StarkNet core contract.
+        starknetCore.sendMessageToL2(l2Address, MINT_SELECTOR, payload);
+
+        //send back the eth - this is just an experiment
+        payable(msg.sender).transfer(PRICE);
+    }
+}
